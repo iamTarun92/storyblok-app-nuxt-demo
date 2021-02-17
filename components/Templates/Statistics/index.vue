931592044ce60eabc51c4f15106a838203fb336c
iamTarun92 <iamTarun92@users.noreply.github.com>
2021-02-17 21:00:03 +0530

    asas

--- components/Templates/Statistics/index.vue
+++ components/Templates/Statistics/index.vue
@@ -1,12 +1,11 @@
 <template>
-  <div :id="blok.id" class="flex flex-wrap">
-    <div
-      v-for="item in blok"
-      :key="item._uid"
-      :class="rowClasses()"
-      class="text-center"
-    >
-      <div v-if="item.number" v-html="item.number" class="font-medium text-4xl"></div>
+  <div :id="blok.id" class="flex flex-wrap text-center">
+    <div v-for="item in blok" :key="item._uid" :class="rowClasses()">
+      <div
+        v-if="item.number"
+        v-html="item.number"
+        class="font-medium text-4xl"
+      ></div>
       <div v-if="item.title" v-html="item.title" class="font-medium"></div>
     </div>
   </div>
@@ -22,7 +21,7 @@ export default {
   },
   methods: {
     rowClasses() {
-      const mdRowWidth = Math.ceil(12 / this.blok.length-1)
+      const mdRowWidth = Math.ceil(12 / this.blok.length - 1)
 
       return `w-1/${mdRowWidth}`
     },
